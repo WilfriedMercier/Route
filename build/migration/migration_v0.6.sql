a361f5136aa197ecf48ba46bfab4225260627659
Wilfried <wifried.mercier.pro@pm.me>
2026-08-14 13:37:12 +0200

    Modified the migration script to match the new db layout

--- build/migration/migration_v0.6.sql
+++ build/migration/migration_v0.6.sql
@@ -2,14 +2,46 @@
 ALTER TABLE hikes
 ADD COLUMN IF NOT EXISTS color VARCHAR(7) DEFAULT '#000000';
 
--- Add a color column for the hikes in the magic links
+-- Rename the magic link table to a clearer name
 ALTER TABLE magic_links
+RENAME TO magic_links_props;
+
+-- Create a new table containing all the magic links
+DROP TABLE IF EXISTS magic_links;
+CREATE TABLE magic_links (
+    id TEXT PRIMARY KEY
+);
+
+-- Add a color column for the hikes in the magic links props table
+ALTER TABLE magic_links_props
 ADD COLUMN IF NOT EXISTS color VARCHAR(7) DEFAULT '#000000';
 
 -- Remove the primary key constraint that worked before when there was one hike per magic link
-ALTER TABLE magic_links
+ALTER TABLE magic_links_props
 DROP CONSTRAINT magic_links_pkey;
 
+-- Remove the old foreign key constraint on the hike id
+ALTER TABLE magic_links_props
+DROP CONSTRAINT fk_bridge_hike;
+
+-- Add a new foreign key constraint on the hike id
+ALTER TABLE magic_links_props
+ADD CONSTRAINT fk_bridge_hike_id
+FOREIGN KEY (hike_id) REFERENCES hikes(id)
+ON DELETE CASCADE;
+
+-- Add the magic links already present in the old magic_links table in the new one
+INSERT INTO magic_links (id)
+SELECT DISTINCT id
+FROM magic_links_props;
+
+-- Add a new foreign key constraint on the magic link
+ALTER TABLE magic_links_props
+ADD CONSTRAINT fk_bridge_magic_links
+FOREIGN KEY (id) REFERENCES magic_links(id)
+ON DELETE CASCADE;
+
 -- Add a new primary key that combines the magic link (id) with the hike id
-ALTER TABLE magic_links 
-ADD CONSTRAINT magic_links_pkey PRIMARY KEY (id, hike_id);
+ALTER TABLE magic_links_props
+ADD CONSTRAINT magic_links_props_pkey 
+PRIMARY KEY (id, hike_id);
